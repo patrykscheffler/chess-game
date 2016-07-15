0c19b9b1593a3f5cf04a8a2efe4d2d4851c54379
Patryk Scheffler <patryk.scheffler@gmail.com>
2016-07-15 13:48:28 +0200

    add Renderer class

--- src/main/java/engineTester/MainGameLoop.java
+++ src/main/java/engineTester/MainGameLoop.java
@@ -2,6 +2,9 @@ package engineTester;
 
 import org.lwjgl.opengl.Display;
 import renderEngine.DisplayManager;
+import renderEngine.Loader;
+import renderEngine.RawModel;
+import renderEngine.Renderer;
 
 public class MainGameLoop {
 
@@ -9,10 +12,28 @@ public class MainGameLoop {
 
         DisplayManager.createDisplay();
 
+        Loader loader = new Loader();
+        Renderer renderer = new Renderer();
+
+        float[] vertices = {
+                -0.5f, 0.5f, 0f,
+                -0.5f, -0.5f, 0f,
+                0.5f, -0.5f, 0f,
+                0.5f, -0.5f, 0f,
+                0.5f, 0.5f, 0f,
+                -0.5f, 0.5f, 0f
+        };
+
+        RawModel model = loader.loadToVAO(vertices);
+
         while (!Display.isCloseRequested()) {
+            renderer.prepare();
+
+            renderer.render(model);
             DisplayManager.updateDisplay();
         }
 
+        loader.cleanUp();
         DisplayManager.closeDisplay();
 
     }
