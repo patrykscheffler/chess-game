e34adbda2b99e75b887bbb8f4d827150ecda5e0e
Patryk Scheffler <patryk.scheffler@gmail.com>
2016-07-15 17:44:21 +0200

    added fragment and vertex shaders

---
+++ src/main/java/shaders/StaticShader.java
@@ -0,0 +1,19 @@
+package shaders;
+
+public class StaticShader extends ShaderProgram {
+
+    public static final String VERTEX_FILE = "src/main/java/shaders/vertexShader.txt";
+    public static final String FRAGEMENT_FILE = "src/main/java/shaders/fragmentShader.txt";
+
+    public StaticShader() {
+        super(VERTEX_FILE, FRAGEMENT_FILE);
+    }
+
+    @Override
+    protected void bindAttributes() {
+
+        super.bindAttribute(0, "position");
+
+    }
+
+}
