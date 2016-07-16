64369c9522123dcb09ecd965270783e18af6b310
Patryk Scheffler <patryk.scheffler@gmail.com>
2016-07-16 14:52:32 +0200

    added model, view and projection matricies

--- src/main/java/shaders/StaticShader.java
+++ src/main/java/shaders/StaticShader.java
@@ -1,12 +1,40 @@
 package shaders;
 
+import entities.Camera;
+import org.lwjgl.util.vector.Matrix4f;
+import toolbox.Maths;
+
 public class StaticShader extends ShaderProgram {
 
     public static final String VERTEX_FILE = "src/main/java/shaders/vertexShader.txt";
     public static final String FRAGEMENT_FILE = "src/main/java/shaders/fragmentShader.txt";
 
+    private int location_transformationMatrix;
+    private int location_projectionMatrix;
+    private int location_viewMatrix;
+
     public StaticShader() {
         super(VERTEX_FILE, FRAGEMENT_FILE);
+    }
+
+    @Override
+    protected void getAllUniformLocation() {
+        location_transformationMatrix = super.getUniformLocation("transformationMatrix");
+        location_projectionMatrix = super.getUniformLocation("projectionMatrix");
+        location_viewMatrix = super.getUniformLocation("viewMatrix");
+    }
+
+    public void loadTransformationMatrix(Matrix4f matrix) {
+        super.loadMatrix(location_transformationMatrix, matrix);
+    }
+
+    public void loadProjectionMatrix(Matrix4f matrix) {
+        super.loadMatrix(location_projectionMatrix, matrix);
+    }
+
+    public void loadViewMatrix(Camera camera) {
+        Matrix4f matrix = Maths.createViewMatrix(camera);
+        super.loadMatrix(location_viewMatrix, matrix);
     }
 
     @Override
